24035ccb672d3add7b6e66ff52c26b690dd256b8
krishnakoushik9 <legionkoushik3@gmail.com>
2025-08-20 14:39:08 +0530

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,27 +4,11 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  //Git Push Updates
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Welcome to Flutter UI world"),
-        ),
         body: Center(
-          child: Container(
-            padding: const EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              color: Colors.redAccent[100],
-              borderRadius: BorderRadius.circular(12),
-              border: Border.all(color: Colors.blue, width: 2),
-            ),
-            child: const Text(
-              "CMRCET",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-          ),
+          child: Text("Hello World"),
         ),
       ),
     );
